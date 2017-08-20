798752ec2eb6dfe827b7366208b3b6205956ca0f
Lainey <laineyNrokk@gmail.com>
2017-08-20 16:39:23 -0700

    put in the main behavioral input-output logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,13 @@
   var number = $("input#chosen").val();
 
   event.preventDefault();
-});
+for (var index = 1; index <= number; index += 1) {
+  if(index % 3 === 0) {$("output").text("Ping");}
+
+    else if (index % 5 === 0) {$("output").text("Pong");}
+
+    else if (index % 15 === 0) {$("output").text("Ping-Pong");}
+
+    else {$("#output").text(index);}
+
+};
